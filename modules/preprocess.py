767cfeeb597ab56d24c610c8a0e7d367adb363c8
Liuchuwei <liuchw3@mail2.sysu.edu.cn>
2023-11-27 23:16:09 +0800

    update

--- modules/preprocess.py
+++ modules/preprocess.py
@@ -2,16 +2,39 @@
 # coding=utf-8
 import argparse
 from argparse import ArgumentDefaultsHelpFormatter
+import numpy as np
 
 def argparser():
+
     parser = argparse.ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter,
         add_help=False
     )
+
+    parser.add_argument("--dataset",
+                        help='Dataset for preprocess. possible dataset: MNIST',
+                        default="MNIST")
+
+    parser.add_argument("--save_dir",
+                        help='Directory to output training results.',
+                        required=True)
 
     return parser
 
 
 def main(args):
 
-    pass
+    "1. load dataset"
+    if args.dataset not in ['MNIST']:
+        raise ValueError("Invalid dataset. possible dataset: MNIST")
+
+    if args.dataset == "MNIST":
+
+        data = np.load("dataset/ADBench/24_mnist.npz", allow_pickle=True)
+        X, y = data['X'], data['y']
+
+    "2. split dataset"
+
+    "3. construct bag dataset"
+
+    "4. save"
