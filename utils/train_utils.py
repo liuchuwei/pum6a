ab14d234f6ae96625b5fee7102f00bfdda07386c
Liuchuwei <liuchw3@mail2.sysu.edu.cn>
2023-12-06 16:55:07 +0800

    update

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -2,6 +2,8 @@ import random
 import numpy as np
 import torch
 from typing import *
+from sklearn.model_selection import StratifiedKFold
+
 
 def set_seed(seed: Optional[int] = 1):
 
@@ -17,6 +19,47 @@ def set_seed(seed: Optional[int] = 1):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+
+def SplitBag(n_splits: Optional[int] = 5,
+             num_bag: Optional[int] = 500,
+             bag_labels: Optional[list] = None
+             ):
+    r"""
+    Instance method to split dataset
+
+        Args:
+            n_splits (int): number of splits of StratifiedKFold
+            num_bag (int): number of total bag datasets
+            bag_labels (list): bag labels
+
+        Return:
+            train_bag_idx (list): list of index of train bag
+            val_bag_idx (list): list of index of validation bag
+            test_bag_idx (list): list of index of test bag
+    """
+
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=666)
+    bag_idx = range(num_bag)
+    bag_label = torch.stack([torch.max(item) for item in bag_labels]).float()
+
+    train_bag_idx = []
+    val_bag_idx = []
+    test_bag_idx = []
+
+    for tmp_train, test in skf.split(bag_idx, bag_label):
+
+        skf1 = StratifiedKFold(n_splits=5, shuffle=True, random_state=666)
+        tmp_label = bag_label[tmp_train]
+
+        test_bag_idx.append(test)
+
+        for train, val in skf1.split(tmp_train, tmp_label):
+            break
+
+        train_bag_idx.append(train)
+        val_bag_idx.append(train)
+
+    return train_bag_idx, val_bag_idx, test_bag_idx
 
 class adanTrainer(object):
 
@@ -52,26 +95,12 @@ class adanTrainer(object):
             else "cpu"
         )
 
+
     def run(self):
 
         "1. Split dataset: 5-fold-cross-validataion"
-        bag_idx = self.bag
-        skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=666)
-
-        # generate mask
-
-        id = range(labels.shape[0])
-        for train1, test in skf.split(id, labels):
-
-            skf1 = StratifiedKFold(n_splits=5, shuffle=True, random_state=666)
-            y1 = labels[train1]
-            x1 = [id[i] for i in train1]
-            idx_test = [id[i] for i in test]
-
-            for train, val in skf1.split(x1, y1):
-                idx_train = [x1[i] for i in train]
-                idx_val =  [x1[i] for i in val]
-                break
+        self.train_idx, self.val_idx, self.test_idx = SplitBag()
 
         "2. Train dataset"
+        
         "3. Evaluate"
